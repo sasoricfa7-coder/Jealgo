e11974376aa8e6751a90fe59291a47c9c5cec38d
marietou334 <chezmarietou@gmail.com>
2026-07-01 06:25:29 +0000

    JeAlgo PWA

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'jealgo-v1';
+const FILES = [
+  '/jealgo/',
+  '/jealgo/index.html',
+  '/jealgo/manifest.json',
+  '/jealgo/profil.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
